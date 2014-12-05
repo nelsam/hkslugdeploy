bb87c879b58a234a688489284b561b30c2f387e2
Samuel Nelson <valczir.darkvein@gmail.com>
2014-12-05 12:02:54 -0700

    Github asset uploading isn't working for some reason.  The exact same (AFAICT) request works via curl, but doesn't work via Go.

--- main.go
+++ main.go
@@ -2,12 +2,18 @@ package main
 
 import (
 	"archive/tar"
+	"bytes"
 	"compress/gzip"
+	"encoding/json"
 	"flag"
+	"fmt"
 	"io"
+	"io/ioutil"
 	"log"
+	"net/http"
 	"path"
 	"strconv"
+	"strings"
 	"time"
 )
 
@@ -22,25 +28,33 @@ const (
 )
 
 var (
-	releaseName       string
+	tarName           string
 	herokuApp         string
+	githubReleaseName string
+	githubReleaseDesc string
 	githubRepo        string
 	githubToken       string
+	gitCommitish      string
 	herokuEmail       string
 	herokuKey         string
 	selectedFilenames []string
 )
 
 func init() {
-	flag.StringVar(&releaseName, "tarball-name", "release.tar.gz",
-		"The name of the release tarball file that will be uploaded "+
-			"to github.")
+	flag.StringVar(&tarName, "tarball-name", "release.tar.gz",
+		"The name of the release tarball file that will be uploaded to github.")
 	flag.StringVar(&herokuApp, "app", "",
 		"Your heroku app's name.")
 	flag.StringVar(&githubRepo, "github-repo", "",
 		"Your github repo, in user/repo form.")
 	flag.StringVar(&githubToken, "github-token", "",
 		"Your github token for pushing a release.")
+	flag.StringVar(&gitCommitish, "github-commitish", "",
+		"The commitish that you're creating a github release of.")
+	flag.StringVar(&githubReleaseName, "github-release-name", "",
+		"The name to use when creating a release on github.")
+	flag.StringVar(&githubReleaseDesc, "github-release-desc", "",
+		"A description of this release for uploading to github.")
 	flag.StringVar(&herokuEmail, "heroku-email", "",
 		"The email address for logging in to heroku.")
 	flag.StringVar(&herokuKey, "heroku-password", "",
@@ -50,7 +64,8 @@ func init() {
 }
 
 func main() {
-	createTarball(releaseName, selectedFilenames)
+	createTarball(tarName, selectedFilenames)
+	githubRelease(githubReleaseName, githubReleaseDesc, gitCommitish, githubRepo, githubToken, tarName)
 }
 
 func createTarball(name string, srcFilenames []string) {
@@ -66,11 +81,19 @@ func createTarball(name string, srcFilenames []string) {
 	if err != nil {
 		log.Fatal(err)
 	}
-	defer targetFile.Close()
 	gz := gzip.NewWriter(targetFile)
-	defer gz.Close()
 	tarball := tar.NewWriter(gz)
-	defer tarball.Close()
+	defer func() {
+		if err := tarball.Close(); err != nil {
+			log.Fatal(err)
+		}
+		if err := gz.Close(); err != nil {
+			log.Fatal(err)
+		}
+		if err := targetFile.Close(); err != nil {
+			log.Fatal(err)
+		}
+	}()
 	writeAppDir(tarball, herokuAppDir)
 	archiveFiles(tarball, srcFiles, herokuAppDir)
 }
@@ -99,7 +122,6 @@ func writeAppDir(tarball *tar.Writer, appDir string) {
 		ChangeTime: now,
 		Typeflag:   tar.TypeDir,
 	}
-	log.Printf("%v", header)
 	tarball.WriteHeader(header)
 }
 
@@ -129,5 +151,77 @@ func archiveFiles(tarball *tar.Writer, srcFiles []os.FileInfo, destPrefix string
 			}
 			archiveFiles(tarball, contents, dstName)
 		}
+	}
+}
+
+func githubRelease(releaseName string, releaseDesc string, commitish string, repo string, token string, attachments ...string) {
+	uploadURL := githubReleaseUploadURL(releaseName, releaseDesc, commitish, repo, token)
+	uploadURL = strings.Replace(uploadURL, "{?name}", "?name=%s", 1)
+	for _, attachment := range attachments {
+		githubUploadAttachment(uploadURL, attachment, token)
+	}
+}
+
+func githubReleaseUploadURL(releaseName string, releaseDesc string, commitish string, repo string, token string) string {
+	bodyMap := map[string]interface{}{
+		"tag_name":         releaseName,
+		"target_commitish": commitish,
+		"name":             releaseName,
+		"body":             releaseDesc,
+		"draft":            true,
+		"prerelease":       true,
+	}
+	bodyBytes, err := json.Marshal(bodyMap)
+	if err != nil {
+		log.Fatal(err)
+	}
+	body := bytes.NewBuffer(bodyBytes)
+	resource := fmt.Sprintf("https://api.github.com/repos/%s/releases", repo)
+	req, err := http.NewRequest("POST", resource, body)
+	if err != nil {
+		log.Fatal(err)
+	}
+	req.Header.Set("Authorization", fmt.Sprintf("token %s", token))
+	req.Header.Set("Content-Type", "application/json")
+	resp, err := new(http.Client).Do(req)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer resp.Body.Close()
+	if resp.StatusCode >= http.StatusBadRequest {
+		log.Fatalf("Github release returned status %s", resp.Status)
+	}
+	respBytes, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		log.Fatal(err)
+	}
+	var respFields map[string]interface{}
+	if err := json.Unmarshal(respBytes, &respFields); err != nil {
+		log.Fatal(err)
+	}
+	return respFields["upload_url"].(string)
+}
+
+func githubUploadAttachment(uploadURL string, attachment string, token string) {
+	resource := fmt.Sprintf(uploadURL, attachment)
+	body, err := os.Open(attachment)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer body.Close()
+	req, err := http.NewRequest("POST", resource, body)
+	if err != nil {
+		log.Fatal(err)
+	}
+	req.Header.Set("Authorization", fmt.Sprintf("token %s", token))
+	req.Header.Set("Content-Type", "application/x-gtar")
+	resp, err := new(http.Client).Do(req)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer resp.Body.Close()
+	if resp.StatusCode >= http.StatusBadRequest {
+		bodyBytes, _ := ioutil.ReadAll(resp.Body)
+		log.Fatalf("Github asset upload return status %s with response:\n%s", resp.Status, string(bodyBytes))
 	}
 }
